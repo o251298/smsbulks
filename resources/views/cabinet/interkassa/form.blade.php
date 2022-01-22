54b1478246eb4f4c73fe09ccb1a1d832985ee4ae
o251298 <o251298@gmail.com>
2022-01-22 13:12:06 +0200

    test interkassa 0.7

--- resources/views/cabinet/interkassa/form.blade.php
+++ resources/views/cabinet/interkassa/form.blade.php
@@ -2,6 +2,7 @@
 @section('title', "Оплата")
 @section('content')
     <form id="payment" name="payment" method="POST" action="https://sci.interkassa.com/" enctype="utf-8">
+        @csrf
         <input type="hidden" name="s" value="6ILrVnps4D" />
         <input type="text" name="ik_am"/>
         <input type="hidden" name="ik_cur" value="uah"/>
